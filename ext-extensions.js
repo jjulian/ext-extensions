b45ea80ced63c21a548204c3ae6be3df50cbdeaa
Jonathan Julian <jonathan.m.julian@gmail.com>
2009-07-20 23:05:08 -0400

    simplify flattenTree

--- ext-extensions.js
+++ ext-extensions.js
@@ -52,22 +52,13 @@ Ext.applyIf(Ext,{
    * Flatten a Node data structure into an array of Nodes. A Node data structure
    * is an array of Nodes, and each node can contain another array of Nodes via
    * the 'children' attribute.
-   *
-   * The new Nodes will only retain attributes 'id', 'text', 'leaf'. If you need other
-   * attributes to be retained, pass their names in an array in extraAttrs.
    */
-  flattenTree: function(treeArray, extraAttrs) {
+  flattenTree: function(treeArray) {
     var a = [];
     Ext.each(treeArray, function(el) {
-      var obj = {id: el.id, text: el.text, leaf: el.leaf};
-      if (extraAttrs) {
-        Ext.each(extraAttrs, function(attr) {
-          obj[attr] = el[attr];
-        });
-      }
-      a.push(obj);
+      a.push(el);
       if (el.children) {
-        a = a.concat(Ext.flattenTree(el.children, extraAttrs));
+        a = a.concat(Ext.flattenTree(el.children));
       }
     });
     return a;
